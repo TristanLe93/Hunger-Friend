b52b0e8c6df4b9a0df7a50b8111140712fb7895d
Tristan Le <tristan.le@student.qut.edu.au>
2014-05-18 23:13:14 +1000

    Added error message: Location Services Disabled
    Error message should show up for disabled Location Services.
    Also, when no results found, it won't transition to results screen

--- OMGAndroid/OMGAndroid/src/main/java/com/example/omgandroid/omgandroid/MainActivity.java
+++ OMGAndroid/OMGAndroid/src/main/java/com/example/omgandroid/omgandroid/MainActivity.java
@@ -20,17 +20,22 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
+/**
+ * This is the initial activity of the app.
+ *
+ * @author Tristan Le
+ */
 public class MainActivity extends Activity implements View.OnClickListener {
-    private Button searchButton;
-
-    // google places api url stuff
     private static final String KEY = "AIzaSyAa2USMUtwlohudtlYIN1Gb7jTYvn5albk";
     private static final String QUERY_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
     private static final String TYPE = "restaurant";
-    private static final String LOCATION = "-27.47,153.02";     // brisbane lng/lat
-    private static final int RADIUS = 500;                      // within 500 meter radius
+    private static final int RADIUS = 500;
 
+    private Button searchButton;
+
+    /**
+     * Set the search button to this listener.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,66 +46,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
         searchButton.setOnClickListener(this);
     }
 
+    /**
+     * On click make a url call to Google Places API
+     */
     @Override
     public void onClick(View view) {
         makeCallToUrl();
-    }
-
-    /**
-     * Makes an API call to the url and gathers the returned JSON formatted data
-     */
-    private void makeCallToUrl() {
-        Location l = getLocationData();
-
-        if (l==null) return;
-
-        String loc = l.getLatitude()+","+l.getLongitude();
-
-        String urlString = QUERY_URL + "location=" + loc + "&radius=" + RADIUS +
-                "&types=" + TYPE + "&sensor=false&key=" + KEY;
-
-        AsyncHttpClient client = new AsyncHttpClient();
-
-        client.get(urlString, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(JSONObject jsonObject) {
-                        startResultsActivity(jsonObject.optJSONArray("results"));
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
-                        Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
-                        Log.e("omg android", statusCode + " " + throwable.getMessage());
-                    }
-                });
-    }
-
-    private Location getLocationData() {
-        LocationManager lm=(LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-        Criteria c=new Criteria();
-        //criteria object will select best service based on
-
-        //now it will first check satellite than Internet than Sim network location
-       String provider=lm.getBestProvider(c, false);
-        //now you have best provider
-        //get location
-        Location l=lm.getLastKnownLocation(provider);
-        if(l!=null)
-        {
-            return l;
-        }
-
-        Toast.makeText(getApplicationContext(), "Cannot get location data. check your connection settings", Toast.LENGTH_LONG);
-        return null;
-
-    }
-    private void startResultsActivity(JSONArray jsonArray) {
-        Intent resultIntent = new Intent(this, ResultsActivity.class);
-
-        resultIntent.putExtra("jsonArray", jsonArray.toString());
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        startActivity(resultIntent);
     }
 
     @Override
@@ -117,4 +68,80 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Makes an API call to the url and returns JSON formatted data
+     */
+    private void makeCallToUrl() {
+        Location l = getLocationData();
+        if (l == null) {
+            return;
+        }
+
+        // combine URL string
+        String loc = "-27.47,153.02";
+        //String loc = l.getLatitude()+","+l.getLongitude();
+        String urlString = QUERY_URL + "location=" + loc + "&radius=" + RADIUS +
+                "&types=" + TYPE + "&sensor=false&key=" + KEY;
+
+        // respond to the client output
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(urlString, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
+                        int results = jsonObject.optJSONArray("results").length();
+
+                        // only transition if we have results
+                        if (results == 0)  {
+                            String msg = "No results were found...";
+                            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                        } else {
+                            startResultsActivity(jsonObject.optJSONArray("results"));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                        // display error message
+                        String msg = "Error: " + statusCode + " " + throwable.getMessage();
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    /**
+     * Retrieves location information from the service provider.
+     * @return Location object containing latitude and longitude
+     */
+    private Location getLocationData() {
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria c = new Criteria();
+
+        // get the location
+        String provider = lm.getBestProvider(c, false);
+        Location location = lm.getLastKnownLocation(provider);
+
+        // display error if we cannot get location data
+        if (location == null) {
+            String msg = "Unable to get location data. Ensure that Location Services are enabled.";
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            return null;
+        }
+
+        return location;
+    }
+
+    /**
+     * Begin the transition to the results screen
+     * @param jsonArray json text containing restaurant information
+     */
+    private void startResultsActivity(JSONArray jsonArray) {
+        Intent resultIntent = new Intent(this, ResultsActivity.class);
+        resultIntent.putExtra("jsonArray", jsonArray.toString());
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(resultIntent);
+    }
+
+
 }
